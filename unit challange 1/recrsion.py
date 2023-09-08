8fd70b4f254c81679f9b7b2c27df53bb1eed127b
sivakumar3331 <144432931+sivakumar3331@users.noreply.github.com>
2023-09-08 14:46:00 +0530

    Add files via upload

---
+++ unit challange 1/recrsion.py
@@ -0,0 +1,16 @@
+# python 3 program to find
+# factorial of given number
+def factorial(n):
+if n<0:
+return 0
+elif n==0 or n==1
+return1
+else:
+    face=1
+    while(n>1):
+    face*=n
+    n-=1
+    return fact
+#Driver code
+num=5
+print("Factorial of ",num,"is",factorial(num)) 
